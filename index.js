6aa924ede89c2cb2f33c0fd170c3afa1d0bd83e9
syntax-co <51330734+syntax-co@users.noreply.github.com>
2024-06-06 10:22:19 -0500

    init

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-// index.js
-// where your node app starts
+
 
 // init project
 var express = require('express');
 var app = express();
 
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+
+
+
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -19,10 +23,19 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+
+
+app.get('/api/:date?',(req,res) => {
+
+
+  console.req.body
+
+
+})
+
+
+
+
 
 
 
